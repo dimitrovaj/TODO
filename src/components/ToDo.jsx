31e53224cba306dc81085dd739bcd5b6fb57d028
dimitrovaj <63523446+dimitrovaj@users.noreply.github.com>
2023-11-21 11:08:00 +0100

    Clear all list, delete todo item, delete multiple (#3)

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import ToDoList from './ToDoList';
+import { v4 as uuidv4 } from 'uuid';
 
 const ToDo = () => {
   const [newTodo, setNewTodo] = useState('');
   const [todos, setTodos] = useState([]);
+  const [selectedItemsArray, setSelectedItemsArray] = useState([]);
 
   const handleInputChange = (event) => {
     setNewTodo(event.target.value);
@@ -11,7 +13,8 @@ const ToDo = () => {
 
   const handleSaveTodo = () => {
     if (newTodo.trim() !== '') {
-      setTodos([...todos, newTodo]);
+      const newTodoObj = { id: uuidv4(), value: newTodo };
+      setTodos([...todos, newTodoObj]);
       setNewTodo('');
     }
   };
@@ -22,6 +25,21 @@ const ToDo = () => {
     }
   };
 
+  const handleDeleteAll = () => {
+    setTodos([]);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const handleDeleteSelectedTodos = () => {
+    const updatedTodos = todos.filter(todo => !selectedItemsArray.includes(todo.id));
+    setTodos(updatedTodos);
+    setSelectedItemsArray([]);
+  };
+
   return (
     <ToDoList
       todos={todos}
@@ -29,6 +47,11 @@ const ToDo = () => {
       handleInputChange={handleInputChange}
       handleOnEnter={handleOnEnter}
       handleSaveTodo={handleSaveTodo}
+      handleDeleteAll={handleDeleteAll}
+      handleDeleteTodo={handleDeleteTodo}
+      selectedItemsArray={selectedItemsArray}
+      setSelectedItemsArray={setSelectedItemsArray}
+      handleDeleteSelectedTodos={handleDeleteSelectedTodos}
     />
   );
 };
