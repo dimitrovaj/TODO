af08bd3af55bfe1576f85bc97c0f6b86c6a98fea
dimitrovaj <63523446+dimitrovaj@users.noreply.github.com>
2023-11-23 15:30:05 +0100

    Local storage (#6)

--- src/ToDoContext.jsx
+++ src/ToDoContext.jsx
@@ -1,13 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const ToDoContext = createContext();
 
 export function ToDoProvider({ children }) {
   const [lists, setLists] = useState([]);
 
+  useEffect(() => {
+    const savedLists = JSON.parse(localStorage.getItem("lists"));
+    if (savedLists) {
+      setLists(savedLists);
+    }
+  }, []);
+
+  // const addToList = (title, newList) => {
+  //   setLists((lists) => [...lists, { title, list: newList }]);
+  // };
 
   const addToList = (title, newList) => {
-    setLists((lists) => [...lists, { title, list: newList }]);
+    const updatedLists = [...lists, { title, list: newList }];
+    setLists(updatedLists);
+    localStorage.setItem("lists", JSON.stringify(updatedLists));
   };
 
 
